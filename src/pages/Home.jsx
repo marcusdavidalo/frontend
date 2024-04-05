d16cb30816dfde9a46abd4789acb68de3ccae49a
Marcus David Alo <marcusdavidalo@gmail.com>
2024-04-05 21:05:54 +0800

    build(deps): update package-lock.json, package.json and Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,22 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Me from "../assets/home/Me.png";
 import useTitle from "../hooks/useTitle";
+import reldocs from "../assets/home/documents.rar";
 
-const Section = ({ title, description, link, linkText }) => (
-  <div className="bg-white p-8 rounded-lg shadow-md transform transition duration-500 ease-in-out hover:scale-105">
-    <h2 className="text-2xl font-bold text-gray-900 mb-4">{title}</h2>
-    <p className="text-gray-600 mb-4">{description}</p>
-    <Link
-      to={link}
-      className="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-    >
-      {linkText}
-    </Link>
+const Section = ({
+  title,
+  description,
+  link,
+  linkText,
+  onClick,
+  isExternal,
+}) => (
+  <div className="bg-white p-6 rounded-lg shadow-md transform transition duration-500 ease-in-out hover:scale-105">
+    <h2 className="text-3xl font-bold text-gray-900 mb-2">{title}</h2>
+    <p className="text-lg text-gray-600 mb-4">{description}</p>
+    {isExternal ? (
+      <a
+        href={link}
+        onClick={onClick}
+        download
+        className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-lg font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+      >
+        {linkText}
+      </a>
+    ) : (
+      <Link
+        to={link}
+        className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-lg font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+      >
+        {linkText}
+      </Link>
+    )}
   </div>
 );
 
 const Home = () => {
   useTitle("Home");
+
   return (
     <div className="bg-gray-100 min-h-max">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
@@ -25,14 +45,15 @@ const Home = () => {
           <img
             src={Me}
             alt="Marcus David Alo"
-            className="mx-auto h-24 w-24 rounded-full"
+            className="mx-auto h-32 w-32 rounded-full"
           />{" "}
           <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
             Marcus David Alo
           </h1>
-          <p className="mt-4 max-w-md mx-auto text-xl text-gray-600">
-            Welcome to my portfolio! I'm a passionate web developer with
-            expertise in React, Tailwind CSS, and modern web technologies.
+          <p className="mt-4 max-w-lg mx-auto text-xl text-gray-600">
+            I am an enthusiastic beginner web developer with a solid grasp of
+            JavaScript, React, and modern web technologies. I'm passionate about
+            continuously expanding my skills in web development.
           </p>
         </div>
 
@@ -56,6 +77,14 @@ const Home = () => {
             description="Get in touch with me for potential collaborations, job opportunities, or general inquiries."
             link="/contact"
             linkText="Contact"
+          />
+
+          <Section
+            title="Relevant Documents"
+            description="Click the button below to download a zip file of relevant documents."
+            link={reldocs}
+            linkText="Download Documents"
+            isExternal={true}
           />
         </div>
       </div>

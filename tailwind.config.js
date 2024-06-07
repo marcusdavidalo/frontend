717c689b7291284b7f92f7484e9a3b0436dbba7e
Marcus David Alo <marcusdavidalo@gmail.com>
2024-06-07 20:47:52 +0800

    Added slideIn custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,27 @@ module.exports = {
   ],
   theme: {
     extend: {
+      transitionProperty: {
+        width: "width",
+      },
       animation: {
         scroll: "scroll 2s linear infinite",
         fadeIn: "fadeIn 2s ease-in forwards",
         fadeOut: "fadeOut 2s ease-in forwards",
         float: "float 3s infinite",
+        slideIn: "slide-in 0.3s ease-out",
       },
       keyframes: {
+        slideIn: {
+          "0%": {
+            transform: "translateX(-100%)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateX(0)",
+            opacity: "1",
+          },
+        },
         fadeIn: {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },

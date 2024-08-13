3b016027e3e8455c4c1ac6b1fb8a6c362ffef10a
Marcus David Alo <marcusdavidalo@gmail.com>
2024-08-13 09:55:15 +0800

    ui update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,81 +11,82 @@ const Contact = () => {
   };
 
   return (
-    <div className="bg-zinc-100 dark:bg-zinc-900  min-h-max">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-4xl font-extrabold text-zinc-900 dark:text-zinc-200 mb-8">
-          Contact Me
+    <div className="bg-zinc-100 dark:bg-zinc-900 h-[75vh] flex items-center justify-center py-12">
+      <div className="w-full max-w-3xl mx-auto p-6 lg:p-10 bg-white dark:bg-zinc-950 border border-zinc-300 dark:border-zinc-800 rounded-xl shadow-lg dark:shadow-black/70 transition-transform transform ease-in-out duration-300">
+        <h1 className="text-4xl font-semibold text-zinc-900 dark:text-zinc-200 mb-10 text-center">
+          Get in Touch
         </h1>
-        <div className="bg-white dark:bg-zinc-950 border border-zinc-300 dark:border-zinc-800 p-6 rounded-lg shadow-md dark:shadow-black/70 transform transition duration-500 ease-in-out ">
-          <form
-            action="https://api.web3forms.com/submit"
-            method="POST"
-            onSubmit={handleSubmit}
-          >
-            <input
-              type="hidden"
-              name="access_key"
-              value={process.env.REACT_APP_WEB3FORMS}
-            />
-            <div className="flex w-full justify-evenly gap-2">
-              <div className="mb-4 w-full">
-                <label
-                  htmlFor="name"
-                  className="block text-zinc-700 dark:text-zinc-300 font-bold mb-2"
-                >
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="bg-white dark:bg-zinc-800 shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 dark:text-zinc-300 leading-tight focus:outline-none focus:shadow-outline"
-                  placeholder="Enter your name"
-                  required
-                />
-              </div>
-              <div className="mb-4 w-full">
-                <label
-                  htmlFor="email"
-                  className="block text-zinc-700 dark:text-zinc-300 font-bold mb-2"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="bg-white dark:bg-zinc-800 shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 dark:text-zinc-300 leading-tight focus:outline-none focus:shadow-outline"
-                  placeholder="Enter your email"
-                  required
-                />
-              </div>
-            </div>
-            <div className="mb-4">
+        <form
+          action="https://api.web3forms.com/submit"
+          method="POST"
+          onSubmit={handleSubmit}
+          className="space-y-6"
+        >
+          <input
+            type="hidden"
+            name="access_key"
+            value={process.env.REACT_APP_WEB3FORMS}
+          />
+          <div className="flex flex-col lg:flex-row lg:gap-6">
+            <div className="flex-1">
               <label
-                htmlFor="message"
-                className="block text-zinc-700 dark:text-zinc-300 font-bold mb-2"
+                htmlFor="name"
+                className="block text-zinc-700 dark:text-zinc-300 text-sm font-medium mb-2"
               >
-                Message
+                Name
               </label>
-              <textarea
-                id="message"
-                name="message"
-                rows="5"
-                className="bg-white dark:bg-zinc-800 shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 dark:text-zinc-300 leading-tight focus:outline-none focus:shadow-outline"
-                placeholder="Enter your message"
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="bg-zinc-50 dark:bg-zinc-800 border border-zinc-300 dark:border-zinc-700 rounded-md w-full py-3 px-4 text-zinc-800 dark:text-zinc-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                placeholder="Enter Name"
                 required
-              ></textarea>
+              />
             </div>
+            <div className="flex-1 mt-6 lg:mt-0">
+              <label
+                htmlFor="email"
+                className="block text-zinc-700 dark:text-zinc-300 text-sm font-medium mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="bg-zinc-50 dark:bg-zinc-800 border border-zinc-300 dark:border-zinc-700 rounded-md w-full py-3 px-4 text-zinc-800 dark:text-zinc-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                placeholder="email@example.com"
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="message"
+              className="block text-zinc-700 dark:text-zinc-300 text-sm font-medium mb-2"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows="6"
+              className="bg-zinc-50 dark:bg-zinc-800 border border-zinc-300 dark:border-zinc-700 rounded-md w-full py-3 px-4 text-zinc-800 dark:text-zinc-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              placeholder="Write your message here..."
+              required
+            ></textarea>
+          </div>
+          <div className="flex justify-center">
             <button
               type="submit"
-              className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-indigo-600 hover:bg-indigo-700 text-white font-semibold py-3 px-8 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 transition-transform transform hover:scale-105"
               disabled={isSubmitting}
             >
               {isSubmitting ? "Sending..." : "Submit"}
             </button>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   );
